7bd3a3f71b3129765c06dc9a321d85ebaf9d3dfb
Dan Cripps <dan-cripps@hotmail.co.uk>
2020-06-05 14:53:06 +0100

    Register component / form complete

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'
+import { Grid, Header, Message, Icon, Form, Segment, Button,  } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 class Register extends React.Component {
+    state = {
+
+    }
+
+    handleChange = () => {
+
+    }
     render() {
         return(
-            <div>Register</div>
+            <Grid textAlign='center' verticalAlign='middle'>
+                <Grid.Column style={{ maxWidth: 450}}>
+                    <Header as='h2' icon color='Black' textAlign='center'>
+                        <Icon name='puzzle piece' color='Black'/>
+                        Register for ChatBox
+                    </Header>         
+                    <Form size='large'>
+                        <Segment stacked>
+                            <Form.Input fluid name='username' icon='user' iconPosition='left' 
+                            placeholder='Username' onChange={this.handleOnChange} type='text'/>
+                            
+                            <Form.Input fluid name='email' icon='mail' iconPosition='left' 
+                            placeholder='Email Address' onChange={this.handleOnChange} type='email'/>
+
+                            <Form.Input fluid name='password' icon='lock' iconPosition='left' 
+                            placeholder='Password' onChange={this.handleOnChange} type='password'/>
+
+                            <Form.Input fluid name='passwordConfirmation' icon='repeat' iconPosition='left' 
+                            placeholder='Password Confirmation' onChange={this.handleOnChange} type='password'/>
+
+                            <Button color='black' fluid size='large'>Submit</Button>
+                        </Segment>
+                    </Form>
+                    <Message>Are you already a user? <Link to='/login'>Login</Link></Message>          
+                </Grid.Column>
+            </Grid>
         )
     }
 }
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAEFj55E8vWy1hW5YTu2cW8720iP4YSxOo",
+    authDomain: "react-chat-app-ff265.firebaseapp.com",
+    databaseURL: "https://react-chat-app-ff265.firebaseio.com",
+    projectId: "react-chat-app-ff265",
+    storageBucket: "react-chat-app-ff265.appspot.com",
+    messagingSenderId: "851248951401",
+    appId: "1:851248951401:web:e1b599bbfa15038b84f2f8",
+    measurementId: "G-GP92B64HLD"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase
+  
