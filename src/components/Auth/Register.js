f3c237709d8495aa15d947bc7e21c60317dec966
Dan Cripps <dan-cripps@hotmail.co.uk>
2020-06-05 12:33:16 +0100

    enviornment set up complete: initial router set

---
+++ src/components/Auth/Register.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Register extends React.Component {
+    render() {
+        return(
+            <div>Register</div>
+        )
+    }
+}
+
+export default Register
